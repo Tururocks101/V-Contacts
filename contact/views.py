e46e5c0d108f16d96ceba9073f389072652c5ca6
Tururocks101! <truphosa.ambiyo@student.moringaschool.com>
2022-04-22 12:47:30 +0300

    first commit

---
+++ contact/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render, redirect
+from .models import Contact
+# Create your views here.
+
+
+def index(request):
+    contacts = Contact.objects.all()
+    return render(request, 'index.html', {'contacts': contacts})
+
+
+def addContact(request):
+    if request.method == 'POST':
+        new_contact = Contact(
+            full_name=request.POST['fullname'],
+            relationship=request.POST['relationship'],
+            email=request.POST['email'],
+            phone_number=request.POST['phone-number']
+        )
+        new_contact.save()
+        return redirect('/')
+    return render(request, 'new.html')
+
+
+def contactProfile(request, pk):
+    contact = Contact.objects.get(id=pk)
+    return render(request, 'contact-profile.html', {'contact': contact})
+
+
+def editContact(request, pk):
+    contact = Contact.objects.get(id=pk)
+    return render(request, 'edit.html', {'contact': contact})
